377e4ecc5115a22557ac695e4472d219ec71ac05
panthinp3 <57723973+panthinp3@users.noreply.github.com>
2021-05-15 11:37:27 -0500

    Update input.groovy

--- input.groovy
+++ input.groovy
@@ -18,14 +18,4 @@ def maven_install(){
                 /*sh 'mvn install'*/
 }
 
-def parameter(){
-    parameters{
-        string(name: 'USER_NAME', defaultValue: 'Hari', description: 'This is the name of the employee')
-        booleanParam(name: 'OPEN', defaultValue: 'True', description: 'Is this business open today?')
-        choice(name: 'EMPLOYEE_ID', choices: ['10001', '1002', '1003'], description: 'Pick your emp ID')
-        password(name: 'PASSWORD', defaultValue: '12345', description: 'Enter your password')
-        
-    }
-}
-
 return this
